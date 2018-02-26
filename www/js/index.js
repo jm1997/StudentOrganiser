c98930e04756255ff6532aa1abb1255eba7145e6
J <32362931+jm1997@users.noreply.github.com>
2018-02-26 12:09:41 +0000

    commit end of lesson

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,21 @@
 $(document).on("pagecreate","#homePage",function(){
     
+    //HOME BUTTON
     $('#homeButton').on("click", function(){
     openHome();
     });  
    
+    //DEADLINE BUTTON
     $('#deadlineButton').on("click", function(){
     openDeadlines();
     }); 
     
+    //TIMETABLE BUTTON
     $('#timetableButton').on("click", function(){
     openTimetable();
     }); 
     
+    //SETTINGS BUTTON
     $('#settingsButton').on("click", function(){
     openSettings();
     }); 
@@ -19,19 +23,22 @@ $(document).on("pagecreate","#homePage",function(){
     
 });            
 
-
+//HOME BUTTON
 function openHome() {
 	$.mobile.navigate( "#homePage" );
 }
 
+//DEADLINE BUTTON
 function openDeadlines() {
 	$.mobile.navigate( "#deadlinePage" );
 }
 
+//TIMETABLE BUTTON
 function openTimetable() {
 	$.mobile.navigate( "#timetablePage" );
 }
 
+//SETTINGS BUTTON
 function openSettings() {
 	$.mobile.navigate( "#settingsPage" );
 }

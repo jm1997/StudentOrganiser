2d8e6a5204e519f7a4bf8863ccbcfa7cb9bf6900
J <32362931+jm1997@users.noreply.github.com>
2018-02-26 11:35:52 +0000

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,36 @@
-/*var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+$(document).on("pagecreate","#homePage",function(){
+    
+    $('#homeButton').on("click", function(){
+    openHome();
+    });  
+   
+    $('#deadlineButton').on("click", function(){
+    openDeadlines();
+    }); 
+    
+    $('#timetableButton').on("click", function(){
+    openTimetable();
+    }); 
+    
+    $('#settingsButton').on("click", function(){
+    openSettings();
+    }); 
+    
+});            
 
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+
+function openHome() {
+	$.mobile.navigate( "#homePage" );
 }
 
+function openDeadlines() {
+	$.mobile.navigate( "#deadlinePage" );
+}
 
-// device APIs are available
-//
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
+function openTimetable() {
+	$.mobile.navigate( "#timetablePage" );
+}
 
-
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
-
-	*/
+function openSettings() {
+	$.mobile.navigate( "#settingsPage" );
+}

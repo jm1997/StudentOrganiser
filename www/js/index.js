d0fd781d622e78430b73798457796cc5a4ce5ea5
Jess <aidan@electedtechnologies.co.uk>
2018-04-21 19:13:47 +0100

    fgbhnjm

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,31 @@
 document.addEventListener("deviceready", function () {
     console.log("enhance panel");
     $('#mypanel').enhanceWithin().panel();
-    
-    
+
+
     //OPEN MENU WITH SWIPE
     $('body').on('swiperight', function () {
         console.log("open panel with swipe");
     $('#mypanel').panel('open', '');
     });
-    
+
 });
 
 //create new variable called options
 var options = {
-    date: new Date(),
+    date: new Date(), //sets value of date to current date
     mode: 'datetime'
     allowOldDates: false, //prevents adding a date in the past
     allowFutureDates: true,
 };
- 
+
+//If datepick opens
 function onSuccess(date) {
     alert('Selected date: ' + date);
 }
- 
-function onError(error) { // Android only 
+
+//if datepicker fails
+function onError(error) { // Android only
     alert('Error: ' + error);
 }
 
@@ -33,7 +35,7 @@ function loadDeadlines() {
             $('#date1').val(localStorage.date1);
             $('#name2').val(localStorage.name2);
             $('#date2').val(localStorage.date2);
-        
+
         }
 
 function saveDeadlines() {
@@ -47,53 +49,53 @@ function saveDeadlines() {
             localStorage.name2 = name2;
             localStorage.date2 = date2;
         }
- 
+
 
 $(document).on("pagecreate", function () {
-    
+
     console.log("page created");
-    
+
     //HOME BUTTON
     $('#homeButton').on("click", function () { //run openHome function when home button is clicked
     openHome();
-    });  
-   
+    });
+
     //DEADLINE BUTTON
     $('#deadlineButton').on("click", function () { //run openDeadlines function when deadline button is clicked
     openDeadlines();
-    }); 
-    
+    });
+
     //TIMETABLE BUTTON
     $('#timetableButton').on("click", function(){
     openTimetable();
-    }); 
-    
+    });
+
     //SETTINGS BUTTON
     $('#settingsButton').on("click", function(){
     openSettings();
-    }); 
-    
-    //ADD DEADLINE BUTTON       
+    });
+
+    //ADD DEADLINE BUTTON
     $('#addDeadlineButton').on("click", function(){
     openAddDeadline();
-    }); 
-    
+    });
+
     //SAVE BUTTON
     $('#saveButton').on("click", function(){
     save();
-    }); 
-    
+    });
+
     //CANCEL BUTTON
     $('#cancelButton').on("click", function(){
     cancel();
-    }); 
-    
+    });
+
     //DATE BUTTON
     $('#dateButton').on("click", function(){
     addDate();
-    }); 
-    
-});  
+    });
+
+});
 
 //HOME BUTTON
 function openHome() {
@@ -128,7 +130,8 @@ function openAddDeadline() {
 //ADD DATE BUTTON
 function addDate() {
     console.log("open add date function running");
-    datePicker.show(options, onSuccess, onError);
+    options.date = datePicker.show(options, onSuccess, onError);
+    $.mobile.navigate( "#addDeadlinePage" ); //open add deadline page
 }
 
 //SAVE
